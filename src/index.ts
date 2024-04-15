2ae7a7e35f7ab2c043ddc5d02f036b92e3470077
Kamil Kisiela <kamil.kisiela@gmail.com>
2024-04-15 12:43:10 +0200

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,118 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { GraphQLParams, Plugin, createSchema, createYoga } from 'graphql-yoga';
+// import { createYogaHive, useYogaHive } from '@graphql-hive/client';
+import { ExecutionArgs } from 'graphql';
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+export interface Env {}
+
+let requestCount = 0;
+
+function writeCountValue(req: Request) {
+	const value = requestCount++;
+	req[Symbol.for('requestCount')] = value;
+
+	return value;
 }
+
+function getCountValue(req: Request) {
+	return req[Symbol.for('requestCount')];
+}
+
+function customPlugin(): Plugin {
+	const cache = new WeakMap<
+		Request,
+		{
+			paramsArgs: GraphQLParams;
+			executionArgs?: ExecutionArgs;
+		}
+	>();
+
+	return {
+		onParams(context) {
+			const reqId = getCountValue(context.request);
+			console.log('hook:onParams', reqId);
+
+			cache.set(context.request, {
+				paramsArgs: context.params,
+			});
+		},
+		onExecute() {
+			return {
+				onExecuteDone({ args }) {
+					console.log('hook:onExecuteDone', getCountValue(args.contextValue.request));
+					const record = cache.get(args.contextValue.request);
+					if (!record) {
+						throw new Error('onExecute.onExecuteDone expected a record');
+					}
+
+					record.executionArgs = args;
+				},
+			};
+		},
+		onResultProcess(context) {
+			console.log('hook:onResultProcess', getCountValue(context.request));
+			const record = cache.get(context.request);
+
+			if (!record) {
+				throw new Error('onResultProcess expected a record');
+			}
+
+			if (Object.keys(record).includes('executionArgs')) {
+				console.log('🍺 OK');
+			} else {
+				console.log('💩 NOT OK');
+			}
+		},
+	};
+}
+
+// const hive = createYogaHive({
+// 	enabled: true,
+// 	debug: true,
+// 	printTokenInfo: false,
+// 	token: '626b8d0005e70baae842bee892bd66d2',
+// 	usage: {
+// 		clientInfo() {
+// 			return {
+// 				name: 'GraphQL Yoga and Cloudflare Worker',
+// 				version: '1.0.0',
+// 			};
+// 		},
+// 		exclude: ['IntrospectionQuery'],
+// 	},
+// 	reporting: false,
+// 	autoDispose: false,
+// });
+
+const yoga = createYoga({
+	schema: createSchema({
+		typeDefs: /* GraphQL */ `
+			type Query {
+				hello: String!
+			}
+		`,
+		resolvers: {
+			Query: {
+				hello: () => 'Hello World!',
+			},
+		},
+	}),
+	plugins: [
+		// useYogaHive(hive),
+		customPlugin(),
+	],
+});
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		console.log('GraphQL start');
+		const val = writeCountValue(request);
+		console.log('Req ID', val);
+
+		const response = await yoga.fetch(request, env, ctx);
+
+		console.log('GraphQL done');
+		// ctx.waitUntil(hive.dispose().then(() => console.log('Hive disposed')));
+
+		return response;
 	},
 };
